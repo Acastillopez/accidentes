95447a10d4d4befd3259f19d27c5bf1c66e94093
Acastillopez <aitor.castillo@unavarra.es>
2021-06-08 10:33:58 +0000

    delete files

--- interactions/interacSanciones.R
+++
@@ -1,7 +0,0 @@
-## INTERACCIÓN SANCION - SANCION VELOCIDAD  
-mod_sancionVelInterac = glm(acturismonum~sancion+sancion_velocidad+ sancion:sancion_velocidad+offset(loganyos), family=quasipoisson, data=train)
-mod_sancionVel = glm(acturismonum~sancion+sancion_velocidad+offset(loganyos), family=quasipoisson, data=train)
-anova(mod_sancionVelInterac,test="Chisq")
-summary(mod_sancionVelInterac)$coefficients
-anova(mod_sancionVel, mod_sancionVelInterac,test="Chisq")
-summary(mod_sancionVel)
--- residualAnalysis.R
+++
@@ -1,8 +0,0 @@
-########################
-## RESIDUALES
-########################
-residuales = residuals(mod0)
-residuales = as.data.frame(residuales)
-ggplot(residuales,aes(residuales)) +  geom_histogram(fill='blue',alpha=0.5)
-plot(mod0)
-
